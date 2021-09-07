423cdf0cbd72465665478efec231a3810675c352
VHarastei <60541960+VHarastei@users.noreply.github.com>
2021-09-07 22:40:27 +0300

    installed and configured tailwind and react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
+import { Layout } from 'components/Layout';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="border border-red-500">
+      <Switch>
+        <Route path="/lessons/:theme/:id">theme lesson</Route>
+        <Route path="/lessons">lessons</Route>
+        <Route path="/profile">profile</Route>
+        <Route path="/login">login</Route>
+        <Route path="/register">register</Route>
+        <Route path="/">
+          <Layout />
+        </Route>
+      </Switch>
     </div>
   );
 }
